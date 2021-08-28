5318f87b6e98356c7093195721e6fdf7f589102a
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-28 16:05:14 +0000

    create tables

--- music/models.py
+++ music/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return f"Artist named {self.name}"
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    artist = models.ForeignKey(Artist, on_delete=models.SET_NULL, null=True)
+    url = models.URLField()
+    users_who_liked = models.ManyToManyField(User, related_name="users_who_liked")
+
+    def __str__(self) -> str:
+        return f"Song {self.name} by {self.artist.name}"
