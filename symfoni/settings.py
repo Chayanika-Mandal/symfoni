9df4bb080239ea435c36342009a3c13d586c833f
Chayanika-Mandal <83910917+Chayanika-Mandal@users.noreply.github.com>
2021-09-18 21:01:44 -0700

    Update settings.py

--- symfoni/settings.py
+++ symfoni/settings.py
@@ -1,6 +1,5 @@
 import os
 
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +20,7 @@ else:
     DEBUG = True
 
 if DEBUG:
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["*"]
 else:
     ALLOWED_HOSTS = ["symfoni.herokuapp.com"]
 
