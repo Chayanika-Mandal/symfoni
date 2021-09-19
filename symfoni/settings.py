32f6562402e2c6addd1ff237fd9890327b97869e
IgnisDa <ignisda2001@gmail.com>
2021-09-19 16:13:13 +0000

    add bulma css

--- symfoni/settings.py
+++ symfoni/settings.py
@@ -1,6 +1,7 @@
 import os
-import dj_database_url
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "music",
     "accounts",
+    # django extension
+    "bulma",
 ]
 
 MIDDLEWARE = [
